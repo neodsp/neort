b4dbe4b435e30a2e9eec77fe8e1b632485efcc28
steckes <stephan@neodsp.com>
2024-12-28 01:22:07 +0100

    re-export modules in main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
-use std::fmt::Debug;
-
-use num::Float;
-use realfft::FftNum;
-
-// pub mod audio_processor;
-// #[cfg(any(
-//     feature = "system-audio-cubeb",
-//     feature = "system-audio-juce",
-//     feature = "system-audio-rtaudio"
-// ))]
-// pub mod system_audio;
-
-pub trait Sample: Float + Debug + FftNum + 'static {}
-
-impl Sample for f32 {}
-impl Sample for f64 {}
+pub use neort_blocks::*;
+pub use neort_dsp::*;
